abed41464b656bb70fd39d319c277e7e3f1b7e70
Nisarg <khodkenisarg@gmail.com>
2025-03-01 10:57:22 +0530

    upd

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,31 @@
-use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
+    let secret_key = rand::rng().random_range(1..=100);
+    println!("Secret Number: {secret_key}");
     println!("Wlecome to Guess The Number! Enter a guess:");
 
-    let mut guess = String::new();
+    loop {
+        let mut guess = String::new();
+        std::io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read lines");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read lines");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("Your guess: {}", guess);
+        println!("Your guess: {}", guess);
+
+        match guess.cmp(&secret_key) {
+            Ordering::Less => println!("Too Less!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You Win!!");
+                break;
+            }
+        }
+    }
 }
