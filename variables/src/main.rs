6b3eed0613666724b813cc970da64c6dbb1de5b4
dracuxan <khodkenisarg@gmail.com>
2025-03-03 15:07:41 +0530

    upd

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,8 +1,29 @@
 fn main() {
     let mut x = 10;
     println!("{x}");
+
     x = 5;
+
     println!("x:{}", x);
     const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
-    println!("{THREE_HOURS_IN_SECONDS}")
+    println!("{THREE_HOURS_IN_SECONDS}");
+
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
+    let spaces = "  ";
+    let spaces = spaces.len();
+    println!("{spaces}");
+
+    let guess: u32 = "42".parse().expect("Expected int value");
+    let short: u32 = 42_000;
+    println!("{short}");
+    println!("{guess}");
 }
