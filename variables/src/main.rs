c870836c94cd1729be410c75e7b23e96fc862280
dracuxan <khodkenisarg@gmail.com>
2025-03-04 10:45:44 +0530

    upd

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
     let mut x = 10;
     println!("{x}");
@@ -26,4 +28,25 @@ fn main() {
     let short: u32 = 42_000;
     println!("{short}");
     println!("{guess}");
+
+    let tup = (500, 6.4, 1);
+
+    let y = tup.1;
+
+    println!("The value of y is: {y}");
+
+    println!("Enter the index number: ");
+
+    let a = [3; 5];
+    let mut index = String::new();
+
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Expected a number ");
+
+    let index: usize = index.trim().parse().expect("Expected a number");
+
+    let el = a[index];
+
+    println!("Element at {index} = {el}");
 }
