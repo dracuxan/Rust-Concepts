e6feab5e04110b2257a7ae1e660ab48388e7d44f
dracuxan <khodkenisarg@gmail.com>
2025-03-07 12:57:45 +0530

    upd

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,1 +1,20 @@
-fn main() {}
+use std::io;
+
+fn main() {
+    let mut s = {
+        let mut user_input = String::new();
+        println!("Enter your name:");
+        io::stdin()
+            .read_line(&mut user_input)
+            .expect("Expected a string input.");
+        user_input
+    };
+
+    s = s.trim().to_string();
+
+    println!("Hello {s}!");
+
+    let s1 = String::from("New String!");
+    let s2 = s1;
+    println!("{s2}");
+}
